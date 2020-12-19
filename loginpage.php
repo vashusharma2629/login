616f0107d6d81431fb7e82ca19053372a99e8b72
vashusharma2629 <vashusharma2625@gmail.com>
2020-12-19 23:00:30 +0530

    login

---
+++ loginpage.php
@@ -0,0 +1,36 @@
+
+<?php
+
+print_r($_REQUEST);
+
+if($_REQUEST['user']== 'vashu')
+ echo "<br/>correct username <br/>";
+
+if($_REQUEST['pwd']== '1234')
+ echo "correct password<br/>";
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
